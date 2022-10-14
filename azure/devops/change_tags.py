c054b1c2f75a04cb4df244ceedd98b7367727e89
Mau <mau@qmau.me>
2022-10-15 01:32:05 +0900

    add azure devops change agent tags script

---
+++ azure/devops/change_tags.py
@@ -0,0 +1,106 @@
+#  https://github.com/vntechies/toolbox
+#
+#  License: MIT
+#
+# This script adds tags to a deployment group's machines in bulk
+# Specify the tags to change and the machines to update the tags
+
+import requests
+import json
+
+# These should be configured via pipeline variables or runtime env
+ORGANIZATION = ""
+PROJECT = ""
+# To find this Id, hit the endpoint "https://{ORGANIZATION}.visualstudio.com/{PROJECT}/_apis/distributedtask/deploymentgroups" replacing the project with the one you're looking for the deployment group in
+DEPLOYMENT_GROUD_ID = 123
+AUTH_TOKEN = ""
+# Comma-separated  list of tags to add (e.g., "tag1,tag2,tag3")
+TAGS_TO_ADD = ""
+# Comma-separated  list of tags to delete (e.g., "tag1,tag2,tag3")
+TAGS_TO_DELETE = ""
+# Comma-separated list of names of the machines that need to change (e.g., "machine1, machine2")
+MACHINES_TO_CHANGE = ""
+DG_API_URL = f"https://dev.azure.com/{ORGANIZATION}/{PROJECT}/_apis/distributedtask/deploymentgroups/{DEPLOYMENT_GROUD_ID}/targets?api-version=6.0-preview.1"
+HEADER = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Accept': 'application/json',
+    'Authorization': AUTH_TOKEN,
+}
+
+# This gets all the machines that belong to the specified deployment group
+def get_all_machine(url, header):
+    print(f"Target machines defs API:{url}")
+
+    response = requests.request(
+        "GET", url, headers=header)
+
+    if response.status_code != 200:
+        error_message = "Expected status code 200, but got %d, with response: %s" % (
+            response.status_code, response.text)
+        raise requests.ConnectionError(error_message)
+
+    return response
+
+# This update the configured machines with the updated tags
+def update_tags(url, header, payload):
+
+    response = requests.request(
+        "PATCH", url, headers=header, data=payload.encode('utf-8'))
+
+    if response.status_code != 200:
+        error_message = "Expected status code 200, but got %d, with response: %s" % (
+            response.status_code, response.text)
+        raise requests.ConnectionError(error_message)
+
+    return response
+
+
+def main():
+    response = get_all_machine(DG_API_URL, HEADER)
+    machines = json.loads(response.text)['value']
+
+    machines_to_change = MACHINES_TO_CHANGE.split(',')
+    tags_to_add = TAGS_TO_ADD.split(',')
+    tags_to_delete = TAGS_TO_DELETE.split(',')
+    updated_machines = []
+
+    for machine in machines:
+        machine_name = machine['agent']['name']
+        if machine_name in machines_to_change:
+            current_tags = machine['tags']
+            print("*"*30)
+            print(f"Updating tag(s) on machine {machine_name}")
+            print(f"Current tag(s): {','.join(current_tags)}")
+
+            updated_tags = current_tags
+
+            if len(tags_to_add) > 0 and tags_to_add[0] != '':
+                print(f"Adding tag(s): {','.join(tags_to_add)}")
+                for tag in tags_to_add:
+                    if tag not in current_tags:
+                        updated_tags.append(tag)
+
+            if len(tags_to_delete) > 0 and tags_to_delete[0] != '':
+                print(f"Deleting tag(s): {','.join(tags_to_delete)}")
+                updated_tags = [
+                    tag for tag in updated_tags if tag not in tags_to_delete]
+
+            updated_machines.append(
+                {'tags': updated_tags, 'id': machine['id']})
+
+    json_data = json.dumps(updated_machines)
+    response = update_tags(DG_API_URL, HEADER, json_data)
+    machines = json.loads(response.text)['value']
+
+    for machine in machines:
+        print('-'*20)
+        print(
+            f"Machine ID: {machine['id']} | Machine name: {machine['agent']['name']}")
+        print(
+            f"Added tags: {','.join(machine['addedTags'])} / Deleted tags: {','.join(machine['deletedTags'])}")
+    print("The tag(s) have been updated!")
+    print("*"*30)
+
+
+if __name__ == "__main__":
+    main()
---
+++ azure/service_bus/purge_dlq.py
@@ -0,0 +1,59 @@
+#  https://github.com/vntechies/toolbox
+#
+#  License: MIT
+#
+# This script purges all messages in Azure service bus DLQ
+
+from azure.servicebus import ServiceBusMessage, ServiceBusSubQueue, ServiceBusClient
+from azure.servicebus.management import ServiceBusAdministrationClient
+
+# These should be configured via a pipeline variables or runtime env
+CONNETION_STRING = ""
+TOPIC_NAME = ""
+SUBSCRIPTION_NAME = ""
+
+
+def main():
+    try:
+        # get the count of dead letter queue (DLQ) messages at the time that the code start being executed
+        servicebus_admin_client = ServiceBusAdministrationClient.from_connection_string(
+            conn_str=CONNETION_STRING)
+        subscription_properties = servicebus_admin_client.get_subscription_runtime_properties(
+            topic_name=TOPIC_NAME, subscription_name=SUBSCRIPTION_NAME)
+        dlq_messages_count = subscription_properties.dead_letter_message_count
+
+        # no DLQ messages
+        if dlq_messages_count < 1:
+            print("No DLQ messages")
+            return
+
+        servicebus_client = ServiceBusClient.from_connection_string(
+            conn_str=CONNETION_STRING)
+        with servicebus_client:
+            dlq_receiver = servicebus_client.get_subscription_receiver(
+                topic_name=TOPIC_NAME, subscription_name=SUBSCRIPTION_NAME, sub_queue=ServiceBusSubQueue.DEAD_LETTER)
+            with dlq_receiver:
+                deleted_msg_count = 0
+                received_msgs = dlq_receiver.receive_messages(
+                    max_message_count=1000, max_wait_time=10)
+                # DLQ messages might be added to the subscription during the execution,
+                # we have to limit the number of messages to be deleted or this will be an infinity loop
+                while (len(received_msgs) > 0 and deleted_msg_count < dlq_messages_count):
+                    for msg in received_msgs:
+                        # delete DLQ messages
+                        dlq_receiver.complete_message(msg)
+                        deleted_msg_count += 1
+                    received_msgs = dlq_receiver.receive_messages(
+                        max_message_count=1000, max_wait_time=10)
+                if deleted_msg_count > 0:
+                    print("Number of DLQ messages that have been purged: ",
+                          deleted_msg_count)
+        print("DQL Purge is completed.")
+    except Exception as ex:
+        print(ex.message)
+        if "could not be found" not in ex.message and 'Not Found' not in ex.message:
+            raise ex from None
+
+
+if __name__ == "__main__":
+    main()
